72b603f101e03b41ee903aca1ee918eb50f1b1df
Dhruv Sohal <sohaldhruv00@gmail.com>
2026-06-29 20:58:55 +0530

    otp send error3 29-6-26

--- backend/src/services/Email.service.js
+++ backend/src/services/Email.service.js
@@ -1,13 +1,18 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false,          // 587 ke liye false
+    secure: false, 
     requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+    },
+    dnsLookup: (hostname, options, callback) => {
+        // Force IPv4 only
+        dns.lookup(hostname, { family: 4 }, callback);
     },
     connectionTimeout: 30000,
     greetingTimeout: 30000,
